2dd33174273a05e2615c2c1ae7649112d90c5223
isaac <isaaczeledon01@outlook.com>
2026-07-08 19:15:49 -0600

    Primer labo

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ def inicio():
  
 @app.route('/acerca') 
 def acerca(): 
-    return '<h1>Acerca del estudiante</h1><p>Nombre: Escriba aquí su nombre completo Edwin isaac Zeledon.</p>' 
+    return '<h1>Acerca de la aplicación</h1><p>Esta es una aplicación de ejemplo creada con Flask para el laboratorio S07b.</p>'        
+    return '<h1>Acerca del estudiante</h1><p>Nombre: Escriba aquí su nombre completo Edwin isaac Zeledon.</p>'  
+    return '<h1>Acerca del estudiante</h1><p>Nombre: Edwin Isaac Zeledon.</p>'
  
 if __name__ == '__main__': 
     app.run(debug=True) 
